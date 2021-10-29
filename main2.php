9b29478c08231c3a1e412ccd3370f42536dde174
Theo57320 <theoantolinig@gmail?com>
2021-10-29 03:08:12 +0200

    Interface

--- main2.php
+++ main2.php
@@ -5,7 +5,6 @@ echo"\nWelcome to MARIO PIZZA ! \n";
 echo("\nEnter 1 : -for a custom pizza\nEnter 2 : -to see the pizza menu\nEnter 3 : -to cancel\n\n");
 
 $number = (int)readline('Enter the number please :');
-
 switch($number){
     case 1:
         echo ("\nWhat are the ingredients you want for your personalized pizza\n");
