9351181169a91fae02c0445cc9348a950c488afb
Theo57320 <theoantolinig@gmail?com>
2021-10-29 02:49:31 +0200

    Interface

--- main2.php
+++ main2.php
@@ -1,3 +1,8 @@
 <?php
 
-echo"A faire pendant les vacs";
+echo"\nWelcome to MARIO PIZZA ! \n";
+
+echo("\nEnter 1 : -for a custom pizza\nEnter 2 : -to see the pizza menu\nEnter 3 : -to cancel\n\n");
+
+$number = readline('Enter the number please :');
+
