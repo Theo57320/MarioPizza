6d47d5b14cf79ed9303b640a4e17623a4011401c
Theo57320 <theoantolinig@gmail?com>
2021-10-31 17:57:14 +0100

    typepizza

--- pizza.php
+++ pizza.php
@@ -1,12 +1,12 @@
 <?php
 
-$Pizza1 = new Pizza('recipe','Margarita','11,00',40,$PizzaList);
+$Pizza1 = new Pizza('recipe','Margarita','9,00',40,$PizzaList);
 $Pizza1->addIngredient('tomato',$PizzaList);
 $Pizza1->addIngredient('ham',$PizzaList);
 $Pizza1->addIngredient('mushrooms',$PizzaList);
 $Pizza1->addIngredient('mozzarella',$PizzaList);
 
-$Pizza2 = new Pizza('recipe','Thonno','12,00',20,$PizzaList);
+$Pizza2 = new Pizza('recipe','Thonno','11,00',20,$PizzaList);
 $Pizza2->addIngredient('tomato',$PizzaList);
 $Pizza2->addIngredient('tuna',$PizzaList);
 $Pizza2->addIngredient('aul',$PizzaList);
@@ -17,10 +17,10 @@ $Pizza3 = new Pizza('recipe','Sofia','12.5',15,$PizzaList);
 $Pizza3->addIngredient('tomato',$PizzaList);
 $Pizza3->addIngredient('ham',$PizzaList);
 $Pizza3->addIngredient('mushrooms',$PizzaList);
-$Pizza3->addIngredient('egg',$PizzaList);
-$Pizza3->addIngredient('cheese',$PizzaList);
+$Pizza3->addIngredient('eggs',$PizzaList);
+$Pizza3->addIngredient('cheeses',$PizzaList);
 
-$Pizza4 = new Pizza('recipe','4 seasons','12.5',25,$PizzaList);
+$Pizza4 = new Pizza('recipe','4 Seasons','14,00',25,$PizzaList);
 $Pizza4->addIngredient('tomato',$PizzaList);
 $Pizza4->addIngredient('ham',$PizzaList);
 $Pizza4->addIngredient('peppers',$PizzaList);
@@ -29,59 +29,76 @@ $Pizza4->addIngredient('asparagus',$PizzaList);
 $Pizza4->addIngredient('olives',$PizzaList);
 $Pizza4->addIngredient('mozzarella',$PizzaList);
 
-$Pizza5 = new Pizza('recipe','a','12.5',40,$PizzaList);
-$Pizza5->addIngredient('tomate',$PizzaList);
-$Pizza5->addIngredient('radis',$PizzaList);
-$Pizza5->addIngredient('riz',$PizzaList);
-$Pizza5->addIngredient('nutella',$PizzaList);
-$Pizza5->addIngredient('radis',$PizzaList);
+$Pizza5 = new Pizza('recipe','5 Cheeses','15,00',10,$PizzaList);
+$Pizza5->addIngredient('tomato',$PizzaList);
+$Pizza5->addIngredient('raclette',$PizzaList);
+$Pizza5->addIngredient('munster',$PizzaList);
+$Pizza5->addIngredient('goats',$PizzaList);
+$Pizza5->addIngredient('mozzarella',$PizzaList);
+$Pizza5->addIngredient('cheeses',$PizzaList);
+
+$Pizza6 = new Pizza('recipe','Calzone','10,00',15,$PizzaList);
+$Pizza6->addIngredient('tomato',$PizzaList);
+$Pizza6->addIngredient('ham',$PizzaList);
+$Pizza6->addIngredient('mushrooms',$PizzaList);
+$Pizza6->addIngredient('eggs',$PizzaList);
+$Pizza6->addIngredient('mozzarella',$PizzaList);
 
 
-$Pizza2 = new Pizza('recipe','a','12.5',40,$PizzaList);
-$Pizza2->addIngredient('tomate',$PizzaList);
-$Pizza2->addIngredient('radis',$PizzaList);
-$Pizza2->addIngredient('riz',$PizzaList);
-$Pizza2->addIngredient('nutella',$PizzaList);
+$Pizza7 = new Pizza('recipe','Chicken','13,00',40,$PizzaList);
+$Pizza7->addIngredient('tomato',$PizzaList);
+$Pizza7->addIngredient('chicken',$PizzaList);
+$Pizza7->addIngredient('peppers',$PizzaList);
+$Pizza7->addIngredient('olives',$PizzaList);
+$Pizza7->addIngredient('cheeses',$PizzaList);
 
-$Pizza2 = new Pizza('recipe','a','12.5',40,$PizzaList);
-$Pizza2->addIngredient('tomate',$PizzaList);
-$Pizza2->addIngredient('radis',$PizzaList);
-$Pizza2->addIngredient('riz',$PizzaList);
-$Pizza2->addIngredient('nutella',$PizzaList);
+$Pizza8 = new Pizza('recipe','Sea pizza','16,00',5,$PizzaList);
+$Pizza8->addIngredient('tomato',$PizzaList);
+$Pizza8->addIngredient('aul',$PizzaList);
+$Pizza8->addIngredient('mushrooms',$PizzaList);
+$Pizza8->addIngredient('sea food',$PizzaList);
+$Pizza8->addIngredient('olives',$PizzaList);
+$Pizza8->addIngredient('cheeses',$PizzaList);
 
-$Pizza2 = new Pizza('recipe','a','12.5',40,$PizzaList);
-$Pizza2->addIngredient('tomate',$PizzaList);
-$Pizza2->addIngredient('radis',$PizzaList);
-$Pizza2->addIngredient('riz',$PizzaList);
-$Pizza2->addIngredient('nutella',$PizzaList);
 
-$Pizza2 = new Pizza('recipe','a','12.5',40,$PizzaList);
-$Pizza2->addIngredient('tomate',$PizzaList);
-$Pizza2->addIngredient('radis',$PizzaList);
-$Pizza2->addIngredient('riz',$PizzaList);
-$Pizza2->addIngredient('nutella',$PizzaList);
+//
+// Creams
+//
+$Pizza9 = new Pizza('recipe','Lorraine','14,00',10,$PizzaList);
+$Pizza9->addIngredient('creams',$PizzaList);
+$Pizza9->addIngredient('ham',$PizzaList);
+$Pizza9->addIngredient('potato',$PizzaList);
+$Pizza9->addIngredient('onions',$PizzaList);
+$Pizza9->addIngredient('mushrooms',$PizzaList);
+$Pizza9->addIngredient('cheeses',$PizzaList);
 
-$Pizza2 = new Pizza('recipe','a','12.5',40,$PizzaList);
-$Pizza2->addIngredient('tomate',$PizzaList);
-$Pizza2->addIngredient('radis',$PizzaList);
-$Pizza2->addIngredient('riz',$PizzaList);
-$Pizza2->addIngredient('nutella',$PizzaList);
+$Pizza10 = new Pizza('recipe','Flame','7,50',7,$PizzaList);
+$Pizza10->addIngredient('creams',$PizzaList);
+$Pizza10->addIngredient('onions',$PizzaList);
+$Pizza10->addIngredient('bacon',$PizzaList);
+$Pizza10->addIngredient('cheeses',$PizzaList);
 
-$Pizza2 = new Pizza('recipe','a','12.5',40,$PizzaList);
-$Pizza2->addIngredient('tomate',$PizzaList);
-$Pizza2->addIngredient('radis',$PizzaList);
-$Pizza2->addIngredient('riz',$PizzaList);
-$Pizza2->addIngredient('nutella',$PizzaList);
+$Pizza11 = new Pizza('recipe','Salmon','16,00',6,$PizzaList);
+$Pizza11->addIngredient('creams',$PizzaList);
+$Pizza11->addIngredient('salmon',$PizzaList);
+$Pizza11->addIngredient('onions',$PizzaList);
+$Pizza11->addIngredient('dill',$PizzaList);
+$Pizza11->addIngredient('cheeses',$PizzaList);
 
-$Pizza2 = new Pizza('recipe','a','12.5',40,$PizzaList);
-$Pizza2->addIngredient('tomate',$PizzaList);
-$Pizza2->addIngredient('radis',$PizzaList);
-$Pizza2->addIngredient('riz',$PizzaList);
-$Pizza2->addIngredient('nutella',$PizzaList);
+$Pizza12 = new Pizza('recipe','Flame thonno','12',3,$PizzaList);
+$Pizza12->addIngredient('creams',$PizzaList);
+$Pizza12->addIngredient('onions',$PizzaList);
+$Pizza12->addIngredient('tuna',$PizzaList);
+$Pizza12->addIngredient('cheeses',$PizzaList);
 
-$Pizza2 = new Pizza('recipe','a','12.5',40,$PizzaList);
-$Pizza2->addIngredient('tomate',$PizzaList);
-$Pizza2->addIngredient('radis',$PizzaList);
-$Pizza2->addIngredient('riz',$PizzaList);
-$Pizza2->addIngredient('nutella',$PizzaList);
+$Pizza13 = new Pizza('recipe','Capre','13,00',8,$PizzaList);
+$Pizza13->addIngredient('creams',$PizzaList);
+$Pizza13->addIngredient('ham',$PizzaList);
+$Pizza13->addIngredient('mushrooms',$PizzaList);
+$Pizza13->addIngredient('chorizo',$PizzaList);
+$Pizza13->addIngredient('onions',$PizzaList);
+$Pizza13->addIngredient('anchovy',$PizzaList);
+$Pizza13->addIngredient('peppers',$PizzaList);
+$Pizza13->addIngredient('cheeses',$PizzaList);
+
 
