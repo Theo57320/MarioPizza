7a86055dc81c7765c88d83ec4239ec119335c0a4
Theo57320 <theoantolinig@gmail?com>
2021-10-31 17:08:42 +0100

    typepizza

---
+++ pizza.php
@@ -0,0 +1,13 @@
+<?php
+
+$Pizza1 = new Pizza('recipe','Margarita','12.5',40,$PizzaList);
+$Pizza1->addIngredient('tomate',$PizzaList);
+$Pizza1->addIngredient('radis',$PizzaList);
+$Pizza1->addIngredient('blabla',$PizzaList);
+$Pizza1->addIngredient('riz',$PizzaList);
+$Pizza1->addIngredient('nutella',$PizzaList);
+$Pizza2 = new Pizza('recipe','a','12.5',40,$PizzaList);
+$Pizza2->addIngredient('tomate',$PizzaList);
+$Pizza2->addIngredient('radis',$PizzaList);
+$Pizza2->addIngredient('riz',$PizzaList);
+$Pizza2->addIngredient('nutella',$PizzaList);
